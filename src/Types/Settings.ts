97cdf6517c78d410050bfd92156d0acaa7e2595b
Simon Skinner <vultuk28@gmail.com>
2022-01-28 17:01:37 +0000

    Initial Commit

--- src/Types/Settings.ts
+++ src/Types/Settings.ts
@@ -1,2 +1,4 @@
 export type Settings = {
+  secret: string;
+  domain?: string;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
+import { Client as faunaDBClient } from 'faunadb';
 
 import { Settings } from './Types/Settings';
 
 export * from './Types';
+export type FaunaDBClient = faunaDBClient;
 
 declare global {
   namespace Express {
     interface Request {
-      // Additional Request enhancements go in here
-      // template: TemplateInterface;
+      faunaDB: FaunaDBClient;
     }
   }
 }
 
-export default (additionalSettings?: Settings) => (req: Request, res: Response, next: NextFunction) => {
+export default (additionalSettings: Settings) => (req: Request, res: Response, next: NextFunction) => {
   // Set any default settings and append additional settings
   const settings: Settings = {
-    ...additionalSettings
-  }
+    domain: 'db.eu.fauna.com',
+    ...additionalSettings,
+  };
+
+  const database: FaunaDBClient = new faunaDBClient({
+    secret: settings.secret,
+    domain: settings.domain,
+    port: 443,
+    scheme: 'https',
+  });
 
   // Define the object that is set as a Request enhancement
-  // req.template = TemplateObject;
+  req.faunaDB = database;
 
   next();
 };
